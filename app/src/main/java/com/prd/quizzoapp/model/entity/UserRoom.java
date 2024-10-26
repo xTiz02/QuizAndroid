45a8ce001de40c4f52305b6ce6d02c8557c39b74
Piero Alessandro Rodriguez Diaz <u21231696@utp.edu.pe>
2024-10-26 12:47:08 -0500

    Ususario de sala

---
+++ app/src/main/java/com/prd/quizzoapp/model/entity/UserRoom.java
@@ -0,0 +1,74 @@
+package com.prd.quizzoapp.model.entity;
+
+public class UserRoom {
+    private String UUID;
+    private String username;
+    private String description;
+    private String img;
+    private boolean isAdmin;
+
+    public UserRoom(String uuid, String username, String description, String img, boolean isAdmin) {
+        UUID = uuid;
+        this.username = username;
+        this.description = description;
+        this.img = img;
+        this.isAdmin = isAdmin;
+    }
+
+
+    public UserRoom() {
+    }
+
+
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRoom{" +
+                "UUID='" + UUID + '\'' +
+                "username='" + username + '\'' +
+                ", description='" + description + '\'' +
+                ", img=" + img +
+                ", isAdmin=" + isAdmin +
+                '}';
+    }
+}
